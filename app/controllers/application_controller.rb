22832a7ea093074eedc566dbd3f8db16231820cd
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-03-25 00:28:31 -0400

    Add the meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :masquerade_user!
 
-  before_action :prepare_meta_tags, if: "request.get?"
+  before_action :prepare_meta_tags
 
   def prepare_meta_tags(options={})
     site_name   = "NationalFeverTracker.com"
