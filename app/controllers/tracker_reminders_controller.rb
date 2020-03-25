094a251726a6cad46124d716d5a71bafde6cbe95
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-03-25 00:08:12 -0400

    Update the credentials

--- app/controllers/tracker_reminders_controller.rb
+++ app/controllers/tracker_reminders_controller.rb
@@ -3,23 +3,23 @@ class TrackerRemindersController < ApplicationController
 
   before_action :set_tracker_reminder, only: [:show, :edit, :update, :destroy]
 
-  # GET /tracker_reminders
-  def index
-    @pagy, @tracker_reminders = pagy(TrackerReminder.all)
-  end
+  # # GET /tracker_reminders
+  # def index
+  #   @pagy, @tracker_reminders = pagy(TrackerReminder.all)
+  # end
 
-  # GET /tracker_reminders/1
-  def show
-  end
+  # # GET /tracker_reminders/1
+  # def show
+  # end
 
   # GET /tracker_reminders/new
   def new
     @tracker_reminder = TrackerReminder.new
   end
 
-  # GET /tracker_reminders/1/edit
-  def edit
-  end
+  # # GET /tracker_reminders/1/edit
+  # def edit
+  # end
 
   # POST /tracker_reminders
   def create
@@ -32,20 +32,20 @@ class TrackerRemindersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tracker_reminders/1
-  def update
-    if @tracker_reminder.update(tracker_reminder_params)
-      redirect_to @tracker_reminder, notice: 'Tracker reminder was successfully updated.'
-    else
-      render :edit
-    end
-  end
+  # # PATCH/PUT /tracker_reminders/1
+  # def update
+  #   if @tracker_reminder.update(tracker_reminder_params)
+  #     redirect_to @tracker_reminder, notice: 'Tracker reminder was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  # DELETE /tracker_reminders/1
-  def destroy
-    @tracker_reminder.destroy
-    redirect_to tracker_reminders_url, notice: 'Tracker reminder was successfully destroyed.'
-  end
+  # # DELETE /tracker_reminders/1
+  # def destroy
+  #   @tracker_reminder.destroy
+  #   redirect_to tracker_reminders_url, notice: 'Tracker reminder was successfully destroyed.'
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
