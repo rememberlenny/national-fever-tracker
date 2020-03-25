dd7dbff2fe5bb85505b64c322ecce4216bbb541d
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-03-24 23:47:55 -0400

    Update form

--- app/models/temperature_check.rb
+++ app/models/temperature_check.rb
@@ -3,9 +3,9 @@ class TemperatureCheck < ApplicationRecord
   validate :temperature_value_min
   # validates :symptoms, presence: true
   validates :age, presence: true
-  validates :loc_city, presence: true
+  # validates :loc_city, presence: true
   validates :loc_zip, presence: true, length: { minimum: 5 }
-  validates :loc_state, presence: true
+  # validates :loc_state, presence: true
 
   def temperature_value_min
     if !temperature_value.blank? && temperature_value < 90 || temperature_value > 110
