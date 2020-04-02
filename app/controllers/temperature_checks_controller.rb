a109b76de8d53dbac88f6e02f69016b752b69d2c
Aaron Soellinger <free-soellingeraj@Lydias-MacBook-Pro.local>
2020-04-02 10:40:04 +0200

    added back forgery thing

--- app/controllers/temperature_checks_controller.rb
+++ app/controllers/temperature_checks_controller.rb
@@ -1,4 +1,5 @@
 class TemperatureChecksController < ApplicationController
+  protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
 
   # before_action :set_temperature_check, only: [:show, :edit, :update, :destroy]
