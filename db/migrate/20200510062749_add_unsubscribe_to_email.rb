8bb44832bbbc21e5a7d1bdbacb8fd0948253de68
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-05-10 02:39:03 -0400

    Remove the unused one

--- db/migrate/20200510062749_add_unsubscribe_to_email.rb
+++
@@ -1,5 +0,0 @@
-class AddUnsubscribeToEmail < ActiveRecord::Migration[6.0]
-  def change
-    add_column :temperature_checks, :is_unsubscribed, :boolean, default: false
-  end
-end
--- db/migrate/20200510063800_add_unsubscribe_to_tracker_reminder.rb
+++ db/migrate/20200510063800_add_unsubscribe_to_tracker_reminder.rb
@@ -1,6 +1,5 @@
 class AddUnsubscribeToTrackerReminder < ActiveRecord::Migration[6.0]
   def change
     add_column :tracker_reminders, :is_unsubscribed, :boolean, default: false
-    remove_column :temperature_checks, :is_unsubscribed, :boolean, default: false
   end
 end
